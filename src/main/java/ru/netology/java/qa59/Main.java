62ce46eaac7a6098d10bc67a9da358c40f870adc
Pavel Irinarkhov <magicringltd@gmail.com>
2023-07-03 18:54:40 +0600

    add Tests

--- src/main/java/ru/netology/java/qa59/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.netology.java.qa59;
-
-public class Main {
-    public static void main(String[] args) {
-
-        }
-    }
---
+++ src/test/java/ru/netology/java/qa59/TaskTest.java
@@ -0,0 +1,111 @@
+package ru.netology.java.qa59;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+    @Test
+    public void testMatchesForSimpleTaskIfTrue() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean actual = simpleTask.matches("родителям");
+        boolean expected = true;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMatchesForSimpleTaskIfFalse() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean actual = simpleTask.matches("Написать");
+        boolean expected = false;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMatchesEpicIfTrue() {
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        boolean actual = epic.matches("Молоко");
+        boolean expected = true;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMatchesEpicIfFalse() {
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        boolean actual = epic.matches("Мясо");
+        boolean expected = false;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMatchesMeetingTopicIfTrue() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean actual = meeting.matches("Выкатка");
+        boolean expected = true;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMatchesMeetingProjectIfTrue() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean actual = meeting.matches("НетоБанка");
+        boolean expected = true;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMatchesMeetingTopicIfFalse() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean actual = meeting.matches("вторник");
+        boolean expected = false;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMatchesMeetingProjectIfFalse() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean actual = meeting.matches("вторник");
+        boolean expected = false;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
